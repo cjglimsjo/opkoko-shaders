b48921b84653cdf14795cea3c04b0fd2db0d5f03
Joakim Glimsjö <joakim.glimsjo@omegapoint.se>
2018-05-09 22:27:56 +0200

    Shader program 0

--- src/main/java/com/cjglimsjo/opkoko/shaders/demo/Demo.java
+++ src/main/java/com/cjglimsjo/opkoko/shaders/demo/Demo.java
@@ -2,14 +2,31 @@ package com.cjglimsjo.opkoko.shaders.demo;
 
 import com.cjglimsjo.opkoko.shaders.engine.GameLogic;
 import com.cjglimsjo.opkoko.shaders.engine.Window;
+import com.cjglimsjo.opkoko.shaders.engine.graphics.Model;
+import com.cjglimsjo.opkoko.shaders.engine.graphics.ShaderProgram;
 
 import static org.lwjgl.glfw.GLFW.GLFW_KEY_ESCAPE;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL20.GL_FRAGMENT_SHADER;
+import static org.lwjgl.opengl.GL20.GL_VERTEX_SHADER;
 
 public class Demo extends GameLogic {
 
+    private ShaderProgram shaderProgram;
+    private Model model;
+
     @Override
     public void init(Window window) {
+        shaderProgram = new ShaderProgram();
+        shaderProgram.loadShader(GL_VERTEX_SHADER, "shaders/shader.vert");
+        shaderProgram.loadShader(GL_FRAGMENT_SHADER, "shaders/shader.frag");
+        shaderProgram.link();
 
+        model = new Model(new float[]{
+                -0.5f, -0.5f, 0.0f,
+                0.5f, 0.5f, 0.0f,
+                -0.5f, 0.5f, 0.0f
+        });
     }
 
     @Override
@@ -26,11 +43,16 @@ public class Demo extends GameLogic {
 
     @Override
     public void render() {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+        shaderProgram.bind();
+        model.render();
+        shaderProgram.unbind();
     }
 
     @Override
     public void cleanup() {
-
+        model.cleanup();
+        shaderProgram.cleanup();
     }
 }
